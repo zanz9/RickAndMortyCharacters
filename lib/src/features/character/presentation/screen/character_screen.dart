ba626bd8a93130417904f57e1fd9a30f2d5a1efe
zAnz <baidosovich@gmail.com>
2024-09-24 22:59:05 +0500

    ui

--- lib/src/features/character/presentation/screen/character_screen.dart
+++ lib/src/features/character/presentation/screen/character_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kdigital_test/src/core/di/di_init.dart';
+import 'package:kdigital_test/src/features/character/domain/enitity/character_result_entity.dart';
 import 'package:kdigital_test/src/features/character/domain/repository/i_characters_repository.dart';
 import 'package:kdigital_test/src/features/character/presentation/bloc/main_bloc.dart';
 import 'package:kdigital_test/src/features/character/presentation/widgets/character_widget.dart';
@@ -12,19 +13,43 @@ class CharactersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bloc = MainPageBloc(
+      InitialMainPageState(),
+      getIt<ICharactersRepository>(),
+    )..add(const GetTestDataOnMainPageEvent(1));
+
     return Scaffold(
-      body: BlocProvider(
-        create: (context) => MainPageBloc(
-          InitialMainPageState(),
-          getIt<ICharactersRepository>(),
-        )..add(const GetTestDataOnMainPageEvent(1)),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: () {
+                // bloc.add();
+              },
+              child: Container(
+                color: Colors.transparent,
+                child: const Icon(Icons.arrow_back),
+              ),
+            ),
+            Container(
+              color: Colors.transparent,
+              child: const Icon(Icons.arrow_forward),
+            ),
+          ],
+        ),
+      ),
+      body: BlocProvider.value(
+        value: bloc,
         child: BlocConsumer<MainPageBloc, MainPageState>(
           listener: (context, state) {},
           builder: (blocContext, state) {
             if (state is LoadingMainPageState) {
               return const Loader();
             } else if (state is SuccessfulMainPageState) {
-              final result = state.characters;
+              final CharacterResultEntity result = state.characters;
               return ListView.builder(
                 cacheExtent: double.infinity,
                 itemCount: result.characters.length,
--- lib/src/features/character/presentation/widgets/character_widget.dart
+++ lib/src/features/character/presentation/widgets/character_widget.dart
@@ -14,27 +14,63 @@ class CharacterWidget extends StatelessWidget {
       alignment: Alignment.topLeft,
       padding: const EdgeInsets.all(8),
       child: Container(
-        padding: const EdgeInsets.all(16),
-        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
         decoration: ShapeDecoration(
           color: const Color.fromARGB(120, 204, 255, 255),
           shape: ContinuousRectangleBorder(
             borderRadius: BorderRadius.circular(32),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Text(character.name),
-            ),
-            Image.network(
-              character.image,
-              width: 50,
-              height: 50,
+          shadows: const [
+            BoxShadow(
+              color: Color.fromARGB(255, 211, 211, 211),
+              offset: Offset(0, 2),
+              blurRadius: 1,
             ),
           ],
+        ),
+        child: Expanded(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 45,
+                foregroundImage: NetworkImage(character.image),
+              ),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    character.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      character.gender == 'Male'
+                          ? const Icon(Icons.male, color: Colors.blue)
+                          : const Icon(Icons.female, color: Colors.pink),
+                      const SizedBox(width: 5),
+                      Text(character.species,
+                          style: const TextStyle(fontSize: 16)),
+                      const SizedBox(width: 10),
+                      const Text('|'),
+                      const SizedBox(width: 10),
+                      Text('Status: ${character.status}',
+                          style: const TextStyle(fontSize: 16)),
+                    ],
+                  ),
+                  Text('From: ${character.origin['name']}'),
+                  Text(
+                    'Location: ${character.location['name']}',
+                    softWrap: true,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
